9849a7b50fa8b6de949c40c416f5348810517c84
Eduardo Garcia Bautista <eduardo.garciab@hormail.com>
2021-11-27 18:14:01 -0600

    Add iitial styles and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { CodeFormatComponent } from './components/code-format/code-format.component';
+import { SearchComponent } from './components/search/search.component';
+import { InputComponent } from './components/input/input.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HeaderComponent,
+    AvatarComponent,
+    CodeFormatComponent,
+    SearchComponent,
+    InputComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/code-format/code-format.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-format',
+  templateUrl: './code-format.component.html',
+  styleUrls: ['./code-format.component.scss']
+})
+export class CodeFormatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
